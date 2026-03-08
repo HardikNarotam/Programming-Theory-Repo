dadf4e34a774378cf9333bb3f727482404b6f0d3
Hardik <hnarotam1@gmail.com>
2026-03-08 17:46:10 +0000

    DrawPath will display a visual path at gameplay

---
+++ Assets/Scripts/DrawPath.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DrawPath : MonoBehaviour
+{
+
+    public GameObject[] points;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.positionCount = points.Length;
+
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.red;
+
+        lineRenderer.sortingOrder = 1;
+
+        DrawLineThroughPoints();
+    }
+
+    void DrawLineThroughPoints()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lineRenderer.SetPosition(i, points[i].transform.position);
+        }
+    }
+}
